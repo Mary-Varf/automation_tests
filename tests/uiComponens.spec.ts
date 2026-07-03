3a1cbc1b82fe53bd07449a2508320ec5cc72a41b
mary-varf <bma1989@yandex.ru>
2026-07-03 13:18:31 +0100

    ui tests; input, radio, checkbox

---
+++ tests/uiComponens.spec.ts
@@ -0,0 +1,77 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200");
+});
+
+test.describe("Form Layout page", () => {
+  test.beforeEach("", async ({ page }) => {
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("input fields", async ({ page }) => {
+    const usingTheGridEmailInput = page
+      .locator("nb-card", { hasText: "Using the Grid" })
+      .getByRole("textbox", { name: "Email" });
+
+    await usingTheGridEmailInput.fill("test@test.com");
+    await usingTheGridEmailInput.clear();
+
+    await usingTheGridEmailInput.pressSequentially("test@test1.com", {
+      delay: 100,
+    });
+
+    //generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue();
+
+    expect(inputValue).toEqual("test@test1.com");
+
+    //locator assertion
+    await expect(usingTheGridEmailInput).toHaveValue("test@test1.com");
+  });
+
+  test("radio btns", async ({ page }) => {
+    const usingTheGridForm = page.locator("nb-card", {
+      hasText: "Using the Grid",
+    });
+    await usingTheGridForm.getByLabel("Option 1").check({ force: true });
+
+    await usingTheGridForm
+      .getByRole("radio", { name: "Option 2" })
+      .check({ force: true });
+
+    const radioStatus = await usingTheGridForm
+      .getByRole("radio", {
+        name: "Option 2",
+      })
+      .isChecked();
+
+    expect(radioStatus).toBeTruthy();
+    await expect(
+      usingTheGridForm.getByRole("radio", { name: "Option 1" }),
+    ).toBeChecked({ checked: false });
+  });
+});
+
+test("Checkboxes", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Toastr").click();
+
+  await page
+    .getByRole("checkbox", { name: "Hide on click" })
+    .uncheck({ force: true });
+  await page
+    .getByRole("checkbox", { name: "Prevent arising of duplicate toast" })
+    .check({ force: true });
+
+  const allBoxes = page.getByRole("checkbox");
+  for (let box of await allBoxes.all()) {
+    await box.check({ force: true });
+    expect(await box.isChecked()).toBeTruthy();
+  }
+  for (let box of await allBoxes.all()) {
+    await box.uncheck({ force: true });
+    expect(await box.isChecked()).toBeFalsy();
+  }
+});
